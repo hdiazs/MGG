9aff91ca281891a76e10d533fbfe6025ccc33cbb
hdiazs <hdiazl@ipn.mx>
2024-12-17 10:17:13 -0600

    Código para prueba con balance de masa

--- Scripts/mgg.py
+++ Scripts/mgg.py
@@ -27,10 +27,11 @@ thermoprops = pd.read_csv('../CSV/thermodynamic_properties.csv')
 # Propiedades termodinámicas 
 
 R = np.array(8.314) # J/(mol K) Constante de los gases ideales
-T = np.arange(1000,1120,10) # [K]
-# T = np.array(1000)
+# T = np.arange(1000,1120,10) # [K]
+T = np.array(963.15) # [K]
 T = T.reshape(-1,1)
 T0 = np.array(298.15) # K, Temperatura de referencia
+SB = np.array(1.32) # Relación de biomasa/vapor
 
 A = np.array(thermoprops["A"]).reshape(-1,1)
 B = np.array(thermoprops["B"]).reshape(-1,1)
@@ -78,40 +79,66 @@ for i in range(n):
 
 # Siendo reacciones que se llevan a cabo por arriba de los 1000 K, y a presiones atmosféricas se puede considerar como gases ideales
 
+
+Ak = np.array([2.88408925, 11.39263818, 6.406027213]) # Balance de masas atómicas en el sistema antes de la reacción C H O
+
 # Definir el sistema de ecuaciones
-def sistema(vars, DG, T):
+def sistema(vars, DG, T, Ak):
     y1, y2, y3, y4, y5, l1, l2, l3, x = vars 
+    # Modelo
     eq1 = DG[0] + np.log(y1) + l1 / (R * T) + 4 * l2 / (R * T)
     eq2 = DG[1] + np.log(y2) + 2 * l2 / (R * T) + l3 / (R * T)
     eq3 = DG[2] + np.log(y3) + l1 / (R * T) + l3 / (R * T)
     eq4 = DG[3] + np.log(y4) + l1 / (R * T) + 2 * l3 / (R * T)
     eq5 = DG[4] + np.log(y5) + 2 * l2 / (R * T)
-    eq6 = y1 + y3 + y4 - 2 / x
-    eq7 = 4 * y1 + 2 * y2 + 2 * y5 - 14 / x
-    eq8 = y2 + y3 + 2 * y4 - 3 / x
+    
+    # Restricciones
+    eq6 = y1 + y3 + y4 - Ak[0] / x
+    eq7 = 4 * y1 + 2 * y2 + 2 * y5 - Ak[1] / x
+    eq8 = y2 + y3 + 2 * y4 - Ak[2] / x
     eq9 = y1 + y2 + y3 + y4 + y5 - 1
     return [eq1, eq2, eq3, eq4, eq5, eq6, eq7, eq8, eq9]
 
 
-y = np.zeros((n,9)) # Aqui depende del numero de incognitas en el sistema
+# Arreglo para guardar los datos de la solución del sistema
+
+y = np.zeros((n,9)) # Aquí depende del numero de incognitas en el sistema
 
 for i in range(n):
     DG = DGf_RT[i,:]
     Ti = T[i].item()
 
-    # Solución inicial
-    solucion_inicial = [0.02, 0.02, 0.02, 0.02, 0.82, 1, 1, 1, 1]
+    # Valores iniciales
+    valores_iniciales = [0.01, 0.01, 0.01, 0.01, 0.01, 1, 1, 1, 1]
 
     # Resolver el sistema
-    soluciones = fsolve(sistema, solucion_inicial, args=(DG, Ti))
+    soluciones = fsolve(sistema, valores_iniciales, args=(DG, Ti, Ak,))
     y[i] = soluciones
-    solucion_inicial = soluciones
+    valores_iniciales = soluciones
 
 
-plt.plot(T,y[ : , 0 ], c='black', label='CH4')
-plt.plot(T,y[ : , 1 ], c='blue', label='H2O')
-plt.plot(T,y[ : , 2 ], c='red', label='CO')
-plt.plot(T,y[ : , 3 ], c='green', label='CO2')
+
+fmol = ["y_CH4","y_H2O","y_CO","y_CO2","y_H2"]
+print(f"")
+print("-"*37)
+print(f"|{'Syngas composition':^35}|")
+print("-"*37)
+print(f"|{'Temperature, K':^15}|{'Steam/Biomass Ratio':^19}|")
+print("-"*37)
+print(f"|{Ti:^15.2f}|{SB:^19.2f}|")
+print("-"*37)
+print(f"|{'Especies':^15}|{'Fracción mol':^19}|")
+print("-"*37)
+
+for i in range(5):
+    print(f"|{fmol[i]:<15}|{soluciones[i]:>19.4f}|")
+    print("-"*37)
+
+
+# plt.plot(T,y[ : , 0 ], c='black', label='CH4')
+# plt.plot(T,y[ : , 1 ], c='blue', label='H2O')
+# plt.plot(T,y[ : , 2 ], c='red', label='CO')
+# plt.plot(T,y[ : , 3 ], c='green', label='CO2')
 # plt.plot(T,y[ : , 4 ], c='magenta', label='H2')
-plt.legend()
-plt.show()
+# plt.legend()
+# plt.show()
